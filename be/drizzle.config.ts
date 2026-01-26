86c7fa314ae124e1f250fc10f21721f98d26699d
chris <chris552611@gmail.com>
2026-01-26 17:32:32 +0800

    fix: docker config

--- be/drizzle.config.ts
+++ be/drizzle.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    host: process.env.DB_HOST!,
+    port: parseInt(process.env.DB_PORT || "5432"),
+    database: process.env.DB_NAME!,
+    ssl: false,
   },
 });
--- be/src/db/index.ts
+++ be/src/db/index.ts
@@ -6,7 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || "5432"),
 });
 
 export const db = drizzle(pool, { schema });
