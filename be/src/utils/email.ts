2e77667462a858ba44c4fc26a486377504491afa
chris <chris552611@gmail.com>
2026-02-06 00:38:01 +0800

    admin css

--- be/src/utils/email.ts
+++ be/src/utils/email.ts
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE, // e.g., 'gmail'
     host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || "587"),
-    secure: false, // true for 465, false for other ports
+    port: parseInt(process.env.SMTP_PORT || "465"),
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
