0fcc2a9ae286db8f0f312fa746aafc6c613a7773
Erictsl2000 <tan_shing_li@hotmail.com>
2026-03-21 13:55:36 +0800

    Restaurant options API

--- be/src/routes/restaurantRoutes.ts
+++ be/src/routes/restaurantRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getRestaurants, createRestaurant, getRestaurantBySlug, updateRestaurant, getRestaurantById, createRestaurantByTripAdvisorID, getRestaurantByTripAdvisorID, getRestaurantStatsByGoogleSearchQuery, getRestaurantShortVideosBySearchQuery, updateRestaurantStatus } from "../controllers/restaurantController";
+import { getRestaurants, createRestaurant, getRestaurantBySlug, updateRestaurant, getRestaurantById, createRestaurantByTripAdvisorID, getRestaurantByTripAdvisorID, getRestaurantStatsByGoogleSearchQuery, getRestaurantShortVideosBySearchQuery, updateRestaurantStatus, searchRestaurantOptions } from "../controllers/restaurantController";
 import { requireAdmin } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.get("/", getRestaurants);
+router.get("/options/search", searchRestaurantOptions);
 router.get("/:slug", getRestaurantBySlug);
 router.get("/id/:id", getRestaurantById);
 router.get("/tripadvisor/:tripAdvisorID", requireAdmin, getRestaurantByTripAdvisorID);
