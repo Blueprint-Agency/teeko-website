082b9b58f2538161098f61124e9e44041f3a752e
chris <chris552611@gmail.com>
2026-03-30 00:03:38 +0800

    chore: trigger deployment to test org-level secrets

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,3 +1,4 @@
+// deploy trigger 2026-03-30
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,3 +1,4 @@
+// deploy trigger 2026-03-30
 import { Navigation } from "@/components/layout/Navigation";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { FeaturedSection } from "@/components/sections/FeaturedSection";
